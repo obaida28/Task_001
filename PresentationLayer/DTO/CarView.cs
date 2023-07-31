90ac3cd2394f5c8d9c915fe5cccccec910480ade
obaida28 <obaidaalnaheel283501@gmaul.com>
2023-07-31 11:04:32 +0300

    fix errors in mapping

--- PresentationLayer/DTO/CarView.cs
+++ PresentationLayer/DTO/CarView.cs
@@ -1,13 +1,13 @@
 namespace DTO;
-// public class CarView
-// {
-//     public string CarNumber { get; set; }
-//     public string Type { get; set; }
-//     public decimal EngineCapacity { get; set; }
-//     public string Color { get; set; }
-//     public int DailyRate { get; set; }
-//     public Json? DriverName { get; set; }
-//     public string? CustomerName { get; set; }
-// }
-public record CarView(string CarNumber , string Type , decimal EngineCapacity ,
- string Color , int DailyRate , List<string> DriverName ,  List<string> CustomerName);
+public class CarView
+{
+    public string CarNumber { get; set; }
+    public string Type { get; set; }
+    public decimal EngineCapacity { get; set; }
+    public string Color { get; set; }
+    public int DailyRate { get; set; }
+    public string DriverName { get; set; }
+    public string CustomerName { get; set; }
+}
+// public record CarView(string CarNumber , string Type , decimal EngineCapacity ,
+//  string Color , int DailyRate , List<string> DriverName ,  List<string> CustomerName);
--- PresentationLayer/Mapper/CarProfile.cs
+++ PresentationLayer/Mapper/CarProfile.cs
@@ -15,10 +15,22 @@ public class CarProfile : Profile
                         new DriverCar { DriverId = (int)src.DriverId , CarNumber = src.CarNumber} }
             ));
 
-        CreateMap<Car, CarView>()
-            .ForMember(dest => dest.DriverName, opt => opt.MapFrom(src => 
-                src.DriverCars.Select(dc => dc.Driver.DriverName).ToList()))
-            .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => 
-                src.CustomerCars.Select(dc => dc.Customer.CustomerName).ToList()));
+         CreateMap<Car, CarView>()
+            .ForMember(dest => dest.DriverName, opt => opt.MapFrom(src => getDrivers(src)))
+            .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => getCustomers(src)));
+    }
+    private IEnumerable<string> getDrivers(Car list)
+    {
+        if(list.DriverCars.Any())
+            return list.DriverCars.Select(a => a.Driver.DriverName);
+        IEnumerable<string> res = new[] { "No Driver" };
+        return res;
+    }
+    private IEnumerable<string> getCustomers(Car list)
+    {
+        if(list.CustomerCars.Any())
+            return list.CustomerCars.Select(a => a.Customer.CustomerName);
+        IEnumerable<string> res = new[] { "No Customer" };
+        return res;
     }
 }
