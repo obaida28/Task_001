a46eeef4dc897b71739b263b7f7b33ef8e5bd038
obaida28 <obaidaalnaheel283501@gmaul.com>
2023-07-31 09:38:43 +0300

    edit mapping

--- PresentationLayer/Mapper/CarProfile.cs
+++ PresentationLayer/Mapper/CarProfile.cs
@@ -3,19 +3,22 @@ public class CarProfile : Profile
 {
     public CarProfile()
     {
-        // CreateMap < CarDTO, Car > ()
-        //     .ForMember(dest => dest.Driver , opt => opt.Ignore())
-        //     .ForMember(
-        //         dest => dest.DriverId  ,opt => opt.MapFrom(src => src.DriverId ))
-        //     .ForMember(dest => dest.Customer , opt => opt.Ignore())
-        //     .ForMember(
-        //         dest => dest.CustomerId  ,opt => opt.MapFrom(src => src.CustomerId ));
-        // CreateMap < Car, CarView > ()
-        //     .ForMember(
-        //         dest => dest.DriverName  ,opt => opt.MapFrom(src => 
-        //         (src.Driver != null ? src.Driver.DriverName : "No Driver") ))
-        //     .ForMember(
-        //         dest => dest.CustomerName  ,opt => opt.MapFrom(src => 
-        //         (src.Customer != null ? src.Customer.CustomerName : "No Customer") ));
+        CreateMap < CarDTO, Car > ()
+        .ForMember(
+            dest => dest.CustomerCars  ,opt => opt.MapFrom(src => 
+                new List<CustomerCar> { 
+                        new CustomerCar { CustomerId = (int)src.CustomerId , CarNumber = src.CarNumber} }
+            ))
+        .ForMember(
+            dest => dest.DriverCars  ,opt => opt.MapFrom(src => 
+                new List<DriverCar> { 
+                        new DriverCar { DriverId = (int)src.DriverId , CarNumber = src.CarNumber} }
+            ));
+
+        CreateMap<Car, CarView>()
+            .ForMember(dest => dest.DriverName, opt => opt.MapFrom(src => 
+                src.DriverCars.Select(dc => dc.Driver.DriverName).ToList()))
+            .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => 
+                src.CustomerCars.Select(dc => dc.Customer.CustomerName).ToList()));
     }
 }
